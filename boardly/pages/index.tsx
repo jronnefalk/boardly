b48ab2cf5794582cbb0a22a38fbb96d449d35fff
jronnefalk <julia@ronnefalk.se>
2024-05-02 20:27:35 +0200

    Added auth0 authentication
    Users are now able to login and logout

--- boardly/pages/index.tsx
+++ boardly/pages/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useUser } from "@auth0/nextjs-auth0/client"
 
 function index() {
-  return <div>Index</div>;
+
+  // useUser returns user, error, isLoading
+  const {user, error, isLoading} = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  // If we have an active user in the session
+  if(user) {
+    console.log(user);
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a><br/>
+        Your nickname is {user.name}.
+      </div>
+    )
+  }
+ 
+  
+  return <a href="/api/auth/login">Login</a>;
 }
 
 export default index;
