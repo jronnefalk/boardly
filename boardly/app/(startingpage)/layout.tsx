02bba5e714a9a467543d8469f4cbeff3dda4e73f
jronnefalk <julia@ronnefalk.se>
2024-05-03 23:15:29 +0200

    Initial design for starting page

---
+++ boardly/app/(startingpage)/layout.tsx
@@ -0,0 +1,15 @@
+const StartingPageLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="h-full bg-slate-100">
+            <main className="pt-40 pb-20 bg-slate-100">
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default StartingPageLayout;
--- boardly/app/example/page.tsx
+++
@@ -1,9 +0,0 @@
-const ExamplePage = () => {
-    return (
-        <div>
-            Example page.
-        </div>
-    );
-};
-
-export default ExamplePage;
