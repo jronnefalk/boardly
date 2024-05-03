5f4fbc958dd49baf7a77675affae34faa9bab113
jronnefalk <julia@ronnefalk.se>
2024-05-03 23:50:20 +0200

    Added design for the footer

--- boardly/app/(startingpage)/layout.tsx
+++ boardly/app/(startingpage)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
 
 const StartingPageLayout = ({
@@ -11,6 +12,7 @@ const StartingPageLayout = ({
             <main className="pt-40 pb-20 bg-slate-100">
                 {children}
             </main>
+            <Footer/>
         </div>
     );
 };
--- boardly/app/(startingpage)/page.tsx
+++ boardly/app/(startingpage)/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { Poppins } from "next/font/google";
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button";
 
 const textFont = Poppins({
     subsets: ["latin"],
