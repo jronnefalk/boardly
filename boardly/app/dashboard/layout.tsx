d7b0328bf3e4468118847a74a36074fd67413f0b
jronnefalk <julia@ronnefalk.se>
2024-05-07 01:32:27 +0200

    Design touch ups on the dashboard page

--- boardly/app/dashboard/layout.tsx
+++ boardly/app/dashboard/layout.tsx
@@ -1,12 +1,19 @@
+import Navbar from "@/components/navbar";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-export default function DashboardLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
-            {children}
+        <div className={cn("min-h-screen flex flex-col", textFont.className)}>
+            <Navbar />
+            <div className="flex-grow flex items-center justify-center flex-col pt-16">
+                {children}
+            </div>
         </div>
     );
 }
