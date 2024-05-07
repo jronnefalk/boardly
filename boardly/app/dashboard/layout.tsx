f1e9de4c0f48ee71b430a65bf4ebee33a2aec06c
jronnefalk <julia@ronnefalk.se>
2024-05-07 02:43:15 +0200

    Revert "Design touch ups on the dashboard page"
    This reverts commit d7b0328bf3e4468118847a74a36074fd67413f0b.

--- boardly/app/dashboard/layout.tsx
+++ boardly/app/dashboard/layout.tsx
@@ -1,19 +1,12 @@
-import Navbar from "@/components/navbar";
-import { Poppins } from "next/font/google";
-import { cn } from "@/lib/utils";
 
-const textFont = Poppins({
-    subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
-        <div className={cn("min-h-screen flex flex-col", textFont.className)}>
-            <Navbar />
-            <div className="flex-grow flex items-center justify-center flex-col pt-16">
-                {children}
-            </div>
+        <div>
+            {children}
         </div>
     );
 }
--- boardly/components/authButtons.tsx
+++ boardly/components/authButtons.tsx
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
 import { Button } from "@/components/ui/button";
 import { LoginLink, RegisterLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export const AuthButtons = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
     return (
-        <div className="flex items-center space-x-2">
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
             {isAuthenticated ? (
-                <Button size="sm" variant="ghost" asChild className="bg-gray-100 text-black hover:bg-gray-200">
+                <Button size="sm" variant="outline" asChild>
                     <LogoutLink>Logout</LogoutLink>
                 </Button>
             ) : (
                 <>
-                    <Button size="sm" variant="ghost" asChild className="bg-gray-100 text-black hover:bg-gray-200">
+                    <Button size="sm" variant="outline" asChild>
                         <LoginLink>Login</LoginLink>
                     </Button>
-                    <Button size="sm" variant="ghost" asChild className="bg-gray-100 text-black hover:bg-gray-200">
+                    <Button size="sm" variant="outline" asChild>
                         <RegisterLink>Sign up</RegisterLink>
                     </Button>
                 </>
